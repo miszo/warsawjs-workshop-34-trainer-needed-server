31a75f439423dda9a637d7979ce7b16c41517c05
Jacek Mikrut <jacekmikrut@gmail.com>
2019-07-27 15:18:19 +0200

    [STEP 7] Handle identification data.

--- server.js
+++ server.js
@@ -9,9 +9,46 @@ const webSocketServer = new WebSocket.Server({
 webSocketServer.on('connection', function(webSocket) {
   console.log('client connected');
 
+  const connectedPerson = {
+    role: null,
+    identification: null,
+  };
+
+  const freshConnectionMessagesHandler = {
+    'identification': function(message) {
+      connectedPerson.role           = message.role;
+      connectedPerson.identification = message.identification;
+
+      switch (message.role) {
+        case ROLES.TRAINER:
+          messagesHandler = trainerMessagesHandler;
+          putTrainerOnFreeTrainerList(connectedPerson);
+          break;
+        case ROLES.STUDENT:
+          messagesHandler = studentMessagesHandler;
+          break;
+      }
+
+      printCurrentState();
+    },
+  };
+
+  const studentMessagesHandler = {
+  };
+
+  const trainerMessagesHandler = {
+  };
+
+  let messagesHandler = freshConnectionMessagesHandler;
+
   webSocket.on('message', function(json) {
     const message = JSON.parse(json);
     console.log('received message:', message);
+
+    const messageHandler = messagesHandler[message.type];
+    if (messageHandler) {
+      messageHandler(message);
+    }
   });
 
   webSocket.on('close', function() {
