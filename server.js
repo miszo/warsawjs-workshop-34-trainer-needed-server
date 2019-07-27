79790fa0f57972a5261f654eee1b1f8962b3eec6
Jacek Mikrut <jacekmikrut@gmail.com>
2019-07-27 14:26:05 +0200

    [STEP 2] Accept incoming connections.

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+const WebSocket = require('ws');
+
+const WEB_SOCKET_PORT = 3333;
+
+const webSocketServer = new WebSocket.Server({
+  port: WEB_SOCKET_PORT,
+});
+
+webSocketServer.on('connection', function() {
+  console.log('client connected');
+});
+
 console.log('TrainerNeeded server...');
